d01f0c056c87f8192266326c1e146bbfeec56eb7
Diya Rajesh Patel <DiyaRajesh.Patel@edu.sait.ca>
2024-01-23 12:08:17 -0700

    update relative link

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ export default function Page() {
   return <>
     <main>CPRG 306: Web Development 2 - Assignments</main>
     <p>
-      <a href="http://localhost:3000/week-2">Week 2 </a>
+      <a href="week-2">Week 2 </a>
     </p>
   </>
 }
