2c8c45d538df047d094bbd75c65a1e6db1e998d4
Diya Rajesh Patel <DiyaRajesh.Patel@edu.sait.ca>
2024-01-26 13:43:15 -0700

    Github repo added

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -2,7 +2,7 @@ export default function StudentInfo() {
     return <>
         <main>Diya Patel</main>
         <p>
-            <a href="https://github.com/DiyarPatel">https://github.com/DiyarPatel</a>
+            <a href="https://github.com/DiyarPatel/cprg306-assignments.git">https://github.com/DiyarPatel/cprg306-assignments.git</a>
         </p>
 
     </>
