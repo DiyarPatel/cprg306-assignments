fa3a5a0cb074447701d16d893c9f35cd61be033c
Diya Rajesh Patel <DiyaRajesh.Patel@edu.sait.ca>
2024-02-06 17:14:53 -0700

    final

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,10 +1,10 @@
 import Itemlist from "./itemlist";
-export default function Item(props) {
+export default function Item() {
     return (
         <div className="p-2.5 ml-5 pt2 border-2 mb-3 w-96 h-20 bg-zinc-800">
-            <li className="font-bold text-xl">{props.name}</li>
+            <li className="font-bold text-xl">{name}</li>
             <li className="">
-                But {props.quantity} in {props.category}
+                But {quantity} in {category}
             </li>
         </div>
     );
