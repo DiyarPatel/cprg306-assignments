03c0bdd901ac86c69c116fdfacc77f100bcdb8a2
Diya Rajesh Patel <DiyaRajesh.Patel@edu.sait.ca>
2024-02-27 17:04:24 -0700

    final

--- app/week-5/item-list.js
+++ app/week-5/item-list.js
@@ -3,8 +3,6 @@ import React, { useState } from 'react';
 import items from './items.json';
 import Item from './item';
 
-
-
 const ItemList = () => {
   const [sortBy, setSortBy] = useState('name');
   const sortedItems = [...items].sort((a, b) => {
@@ -19,9 +17,9 @@ const ItemList = () => {
     return (
       <div>
         <button
-          className='p-3 bg-pink-500 text-black-500'
+          className='p-3 bg-pink-500 text-black-500 border-2 border-pink-500 rounded-md hover:bg-pink-700 '
           onClick={() => setSortBy(value)}
-          style={{ backgroundColor: sortBy === value ? 'lightblue' : 'white' }}
+          style={{ backgroundColor: sortBy === value ? 'lightpink' : 'white' }}
         >
           {lable}
         </button>
@@ -30,7 +28,7 @@ const ItemList = () => {
   };
   return (
     <div>
-      <div>
+      <div className='flex space-x-4'>
         {renderSortButton('Name', 'name')}
         {renderSortButton('Category', 'category')}
       </div>
