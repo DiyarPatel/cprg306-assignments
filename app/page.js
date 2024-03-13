d7869328494fdd8dbb66d49f00c8fc4c17664d7f
Diya Rajesh Patel <DiyaRajesh.Patel@edu.sait.ca>
2024-03-12 23:35:13 -0600

    csschange

--- app/page.js
+++ app/page.js
@@ -1,25 +1,25 @@
 import Link from "next/link";
 
 export default function Page() {
-  return <>
-    <main>CPRG 306: Web Development 2 - Assignments</main>
+  return < >
+    <main className="text-3xl font-bold mb-4">CPRG 306: Web Development 2 - Assignments</main>
     <p>
-      <Link href="week-2">Week 2 </Link>
+      <Link href="week-2" className="text-blue-500 hover:text-blue-700" >Week 2 </Link>
     </p>
     <p>
-      <Link href="week-3">Week 3</Link>
+      <Link href="week-3" className="text-blue-500 hover:text-blue-700">Week 3</Link>
     </p>
     <p>
-      <Link href="week-4">Week 4</Link>
+      <Link href="week-4" className="text-blue-500 hover:text-blue-700">Week 4</Link>
     </p>
     <p>
-      <Link href="week-5">Week 5</Link>
+      <Link href="week-5" className="text-blue-500 hover:text-blue-700">Week 5</Link>
     </p>
     <p>
-      <Link href="week-6">Week 6</Link>
+      <Link href="week-6" className="text-blue-500 hover:text-blue-700">Week 6</Link>
     </p>
     <p>
-      <Link href="week-7">Week 7</Link>
+      <Link href="week-7" className="text-blue-500 hover:text-blue-700">Week 7</Link>
     </p>
   </>
 }
